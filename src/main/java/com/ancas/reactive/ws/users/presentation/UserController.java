6cef04a2d27a8289bde38716bfab1d4450c2c77f
afcastro <afcastro@cotrfa.com.co>
2025-04-21 19:13:00 -0500

    Feat: Create project

---
+++ src/main/java/com/ancas/reactive/ws/users/presentation/UserController.java
@@ -0,0 +1,15 @@
+package com.ancas.reactive.ws.users.presentation;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @PostMapping
+    public void createUser(){
+
+    }
+}
